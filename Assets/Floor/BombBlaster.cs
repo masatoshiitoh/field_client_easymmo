2d2b10bb9caa3869221989f46b5d1e7c43af80df
Masatoshi Itoh <masatoshi9953@gmail.com>
2014-08-31 13:22:34 +0900

    Initial commit

---
+++ Assets/Floor/BombBlaster.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BombBlaster : MonoBehaviour {
+	public GameObject bombType;
+	public AudioClip audioClip;
+
+	void BlastOnce () {
+		Instantiate(bombType, this.transform.position, this.transform.rotation);
+		AudioSource.PlayClipAtPoint(audioClip, new Vector3());
+	}
+}
---
+++ Assets/Floor/BombCreater.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BombCreater : MonoBehaviour {
+	public GameObject bombType;
+	public AudioClip audioClip;
+	private float INTERVAL = 5.0f;
+	public float timer = 0.0f;
+	
+
+	// Use this for initialization
+	void Start () {
+		//timer = 0.0f;
+		INTERVAL = timer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.deltaTime;
+		if(timer < 0.0f){
+			Instantiate(bombType, this.transform.position, this.transform.rotation);
+			AudioSource.PlayClipAtPoint(audioClip, new Vector3());
+			timer = INTERVAL;
+		}
+
+	}
+	void BlastOnce () {
+		Instantiate(bombType, this.transform.position, this.transform.rotation);
+		AudioSource.PlayClipAtPoint(audioClip, new Vector3());
+	}
+}
