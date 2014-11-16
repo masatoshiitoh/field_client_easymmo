cb81fc97607fc5c31f5a401363f43664d0c67bbf
Masatoshi Itoh <masatoshi9953@gmail.com>
2014-11-16 15:46:30 +0900

    Working.

---
+++ Assets/Scripts/ClickTerrain.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickTerrain : MonoBehaviour {
+	public GameObject character;
+	Vector3 worldPoint;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0) == true) {
+
+
+		}
+	}
+}
---
+++ Assets/Scripts/ClickToXZ.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickToXZ : MonoBehaviour {
+	public GameObject character;
+	Vector3 worldPoint;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0) == true) {
+			Vector3 screenPoint = Input.mousePosition;
+			screenPoint.z = 10.0f;
+			worldPoint = camera.ScreenToWorldPoint (screenPoint);
+			Debug.Log (worldPoint);
+
+			if (character != null) {
+				character.transform.position = worldPoint;
+			}
+		}
+	}
+}
